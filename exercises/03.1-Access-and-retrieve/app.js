291fd075328cb4268a3e7fb4ef16cdb5fb35d41c
Margoth <mloreana@gmail.com>
2021-02-22 01:27:40 +0000

    exercise 03.1

--- exercises/03.1-Access-and-retrieve/app.js
+++ exercises/03.1-Access-and-retrieve/app.js
@@ -3,6 +3,13 @@ var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','satu
 
 //1. print the item here
 
+let daysOfWeek = myArray[2];
+console.log(daysOfWeek);
+
+
 //2. change 'thursday'a value here to null
+myArray[4] = null;
+
 
 //3. print the position of step 2
+console.log(myArray[4]);
