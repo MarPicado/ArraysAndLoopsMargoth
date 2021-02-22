25bcb394d5aa5e93bd4df7c8a370a6f6c7bf83e3
Margoth <mloreana@gmail.com>
2021-02-22 02:55:04 +0000

    exercise 06.5

--- exercises/06.2-Loop-from-the-top/app.js
+++ exercises/06.2-Loop-from-the-top/app.js
@@ -1,7 +1,9 @@
-var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+var mySampleArray = [3423, 5, 4, 47889, 654, 8, 867543, 23, 48, 56432, 55, 23, 25, 12];
 
+let lastPosition = mySampleArray.length - 1;
+let stopAt = 0;
 
-for(var i = 0; i<mySampleArray.length; i = i + 1)
+for(var i = lastPosition; i >= stopAt; i = i - 1)
 {
-	console.log(mySampleArray[i]);
+    console.log(mySampleArray[i]);
 }
